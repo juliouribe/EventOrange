8d083c1055ff21930c8fa2766912ac887f4ddaac
Julio Uribe <uribejulioc@gmail.com>
2023-08-14 16:28:31 -0700

    Add changes to make app ready for hosting

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
     resources :users, only: [:show, :create]
     resource :session, only: [:show, :create, :destroy]
   end
+
+  get '*path',
+    to: 'static_pages#frontend',
+    # Only forward for non ajax requests and is an html request.
+    constraints: lambda { |req| !req.xhr? && req.format.html? }
+
+  # Front end routes
+  root to: 'static_pages#frontend'
 end
---
+++ hosting-notes.js
@@ -0,0 +1,17 @@
+/*
+Hosting Mini Lecture
+
+When running locally, we run both a frontend and backend server. Frontend is at
+localhost 3000 and backend is at 5000. We only need a server for frontend locally
+because we are having a live development server to update with our changes. In
+production we don't need a frontend server, just a static html page.
+
+Need to add linux to Gemfile.lock
+bundle lock --add-platform x86_64-linux
+
+Make sure public folder is in .gitignore
+
+
+
+
+*/
