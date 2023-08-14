07f43ea03ad91ba300f5f6e57a6af0290934347b
Julio Uribe <uribejulioc@gmail.com>
2023-08-14 13:17:27 -0700

    Add more validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,14 @@
 class User < ApplicationRecord
   validates :email, :username, :session_token, presence: true
   validates :email, :username, :session_token, uniqueness: true
-  validates :password, length: { minimum: 6}, allow_nil: true
+  validates :username,
+    length: { in: 3..30 },
+    format: { without: URI::MailTo::EMAIL_REGEXP, message: "Cant be an email" }
+  validates :email,
+    length: { in: 3..255},
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password,
+    length: { in: 6..255}, allow_nil: true
 
   has_secure_password
 
--- db/migrate/20230814184049_create_users.rb
+++ db/migrate/20230814184049_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :email, null: false
       t.string :username, null: false
-      t.string :password_digest
+      t.string :password_digest, null: false
       t.string :session_token, null: false
 
       t.timestamps
