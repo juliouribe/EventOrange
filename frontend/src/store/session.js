ce5e922ff1fbfc4952dc3a1cb9efa27fbfd7d2f6
Julio Uribe <uribejulioc@gmail.com>
2023-08-14 19:33:15 -0700

    Add session reducer and thunk actions

---
+++ frontend/src/store/session.js
@@ -0,0 +1,73 @@
+import csrfFetch, { storeCSRFToken, restoreCSRF } from './csrf.js';
+
+export const LOGIN_USER = 'session/LOGIN_USER';
+export const LOGOUT_USER = 'session/LOGOUT_USER';
+
+const loginUser = (user) => ({
+  type: LOGIN_USER,
+  payload: user
+})
+
+const logoutUser = () => ({
+  type: LOGOUT_USER
+})
+
+const storeCurrentUser = user => {
+  if (user) {
+    sessionStorage.setItem('currentUser', JSON.stringify(user));
+  } else {
+    sessionStorage.removeItem('currentUser')
+  }
+}
+
+export const signup = (user) => async dispatch => {
+  const { email, password } = user;
+  const res = await csrfFetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({
+      email,
+      password
+    })
+  });
+  const data = await res.json();
+  storeCurrentUser(data.user);
+  dispatch(loginUser(data.user));
+  return res;
+}
+
+export const login = ({ email, password }) = async dispatch => {
+  const res = await csrfFetch('/api/session', {
+    method: 'POST',
+    body: JSON.stringify({ email, password })
+  });
+  const data = await res.json();
+  storeCurrentUser(data.user);
+  dispatch(loginUser(data.user));
+}
+
+export const logout = () => async dispatch => {
+  const response = await csrfFetch('/api/session', {
+    method: "DELETE"
+  });
+  storeCurrentUser(null);
+  dispatch(logoutUser());
+  return response;
+};
+
+const initialState = {
+  user: JSON.parse(sessionStorage.getItem('currentUser'))
+}
+
+
+const sessionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_USER:
+      return { ...state, currentUser: action.payload.user.id }
+    case LOGOUT_USER:
+      return { ...state, currentUser: null }
+    default:
+      return state
+  }
+}
+
+export default sessionReducer
