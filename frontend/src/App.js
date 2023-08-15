ae163781458334c342f941c4f1e2acab81892e38
Julio Uribe <uribejulioc@gmail.com>
2023-08-15 11:04:24 -0700

    Fix error handling for bad credentials on login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import LoginFormPage from "./components/LoginFormPage";
 import { useDispatch } from "react-redux";
 import * as sessionActions from './store/session'
+import { Route, Switch } from 'react-router-dom';
+
 
 
 function App() {
@@ -13,8 +15,14 @@ function App() {
 
   return (
     <>
-      <LoginFormPage />
-      <button onClick={logout}>Log Out</button>
+      <Switch>
+        <Route path='/login'>
+          <LoginFormPage />
+        </Route>
+        <Route path='/'>
+          <button onClick={logout}>Log Out</button>
+        </Route>
+      </Switch>
       <h1>Lasagna for dinner tonight!!!!</h1>
       <img src="https://i.pinimg.com/474x/3a/dc/92/3adc92551b1495bf31039bc637335603.jpg"></img>
     </>
