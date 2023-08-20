704fad48a52189413c718e960449331861df699e
Julio Uribe <uribejulioc@gmail.com>
2023-08-20 15:35:21 -0700

    Disable rails db commands for deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,7 @@ set -o errexit
 
 npm run build
 bundle install
-rails db:drop
-rails db:create
-rails db:migrate
-rails db:seed #if needed. Remove this when you are super production ready to avoid dropping tables and user accounts.
+# rails db:drop
+# rails db:create
+# rails db:migrate
+# rails db:seed #if needed. Remove this when you are super production ready to avoid dropping tables and user accounts.
