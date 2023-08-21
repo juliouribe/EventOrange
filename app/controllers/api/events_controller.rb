d5f9ff275a4e4fc0035f5a52b0cfc32db5b975d8
Julio Uribe <uribejulioc@gmail.com>
2023-08-21 10:24:37 -0700

    Add create, update, and delete functions to the events controller and redux store

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,4 +1,6 @@
 class Api::EventsController < ApplicationController
+  before_action :require_logged_in, only: [:create, :update, :destroy]
+
   def index
     @events = Event.all
     render :index
@@ -11,15 +13,28 @@ class Api::EventsController < ApplicationController
 
   def create
     # Look up logged in user id when creating events.
-
+    @event = Event.new(events_params)
+    @event.user_id = current_user.id
+    if @event.save
+      render :show
+    else
+      render json: @event.errors.full_messages, status: 422
+    end
   end
 
   def update
-
+    @event = Event.find_by(id: params[:id])
+    if @event.update(events_params)
+      render :show
+    else
+      render json: @event.errors.full_messages, status: 422
+    end
   end
 
   def destroy
-
+    @event = Event.find_by(id: params[:id])
+    @event.destroy
+    render json: event
   end
 
   private
--- frontend/src/store/events.js
+++ frontend/src/store/events.js
@@ -2,6 +2,8 @@ import csrfFetch from "./csrf";
 
 export const RECEIVE_EVENTS = 'events/RECEIVE_EVENTS';
 export const RECEIVE_EVENT = 'events/RECEIVE_EVENT';
+export const UPDATE_EVENT = 'events/UPDATE_EVENT';
+export const REMOVE_EVENT = 'events/REMOVE_EVENT';
 
 // Actions
 const receiveEvents = (events) => ({
@@ -12,6 +14,16 @@ const receiveEvents = (events) => ({
 const receiveEvent = (event) => ({
   type: RECEIVE_EVENT,
   event
+})
+
+const updateEvent = (event) => ({
+  type: UPDATE_EVENT,
+  event
+})
+
+const removeEvent = (eventId) => ({
+  type: REMOVE_EVENT,
+  eventId
 })
 
 // Selectors
@@ -34,13 +46,37 @@ export const fetchEvent = (eventId) => async dispatch => {
   dispatch(receiveEvent(event));
 }
 
+export const createEvent = (event) => async dispatch => {
+  const res = await csrfFetch('/api/events', {
+    method: 'POST',
+    body: JSON.stringify(event)
+  })
+  const newEvent = await res.json();
+  dispatch(receiveEvent(newEvent));
+}
+
+export const editEvent = (event) => async dispatch => {
+  const res = await csrfFetch(`/api/events/${event.id}`, {
+    method: 'PATCH',
+    body: JSON.stringify(event)
+  })
+  const updatedEvent = await res.json();
+  dispatch(updateEvent(updatedEvent));
+}
+
 // Reducer
 const eventReducer = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_EVENTS:
       return { ...action.events }
     case RECEIVE_EVENT:
-      return { ...action.event }
+      return { ...state, [action.event.id]: action.event }
+    case UPDATE_EVENT:
+      return { ...state, [action.event.id]: action.event }
+    case REMOVE_EVENT:
+      const newState = { ...state }
+      delete newState[action.eventId]
+      return newState
     default:
       return state
   }
